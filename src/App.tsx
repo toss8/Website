63280da2ba08a373ddcff20b88a2bc3414506630
Mad-iii <mahdsadiq180@gmail.com>
2026-06-04 21:13:38 +0500

    Shifted images to supabase

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ function AppContent() {
       <footer className="bg-darker py-12 border-t border-white/5">
         <div className="max-w-7xl mx-auto px-4 text-center">
           <img 
-            src="/images/Toss8_Text.png" 
+            src="https://iqknvwaqtdmmtqipacmm.supabase.co/storage/v1/object/public/images/Toss8_Text.png" 
             alt="TOSS 8" 
             className="mb-4 h-20 w-auto object-contain mx-auto"
           />
